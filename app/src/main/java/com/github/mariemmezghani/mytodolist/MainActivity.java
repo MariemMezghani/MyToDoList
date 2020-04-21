c2be7cb9d9caf28269c85385b46a7c73bc343b16
MariemMezghani <35550711+MariemMezghani@users.noreply.github.com>
2020-04-21 00:16:04 -0300

    implement RecyclerView

--- app/src/main/java/com/github/mariemmezghani/mytodolist/MainActivity.java
+++ app/src/main/java/com/github/mariemmezghani/mytodolist/MainActivity.java
@@ -1,14 +1,37 @@
 package com.github.mariemmezghani.mytodolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+    TaskAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set the RecyclerView to its corresponding view
+        mRecyclerView = (RecyclerView) findViewById(R.id.toDoRecyclerView);
+
+        // Set the layout for the RecyclerView to be a linear layout, which measures and
+        // positions items within a RecyclerView into a linear list
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Initialize the adapter and attach it to the RecyclerView
+        mAdapter=new TaskAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
+
+        FloatingActionButton fabButton = (FloatingActionButton) findViewById(R.id.add_fab);
+
     }
 }
---
+++ app/src/main/java/com/github/mariemmezghani/mytodolist/TaskAdapter.java
@@ -0,0 +1,62 @@
+package com.github.mariemmezghani.mytodolist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.github.mariemmezghani.mytodolist.Model.Task;
+
+import java.util.List;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
+    private Context mContext;
+    private List<Task> tasks;
+    public TaskAdapter(Context context){
+        mContext=context;
+    }
+    @NonNull
+    @Override
+    public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Inflate the task_layout to a view
+
+        View view = LayoutInflater.from(mContext)
+
+                .inflate(R.layout.task_layout, parent, false);
+
+
+
+        return new TaskViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TaskViewHolder holder, int position) {
+        Task task=tasks.get(position);
+        String description=task.getDescription();
+        holder.taskDescriptionView.setText(description);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if (tasks==null) {
+            return 0;
+        }
+        return tasks.size();
+    }
+
+    class TaskViewHolder extends RecyclerView.ViewHolder {
+        TextView taskDescriptionView;
+        public TaskViewHolder(View itemView) {
+            super(itemView);
+            taskDescriptionView = (TextView) itemView.findViewById(R.id.taskDescription);
+
+        }
+
+    }
+}
